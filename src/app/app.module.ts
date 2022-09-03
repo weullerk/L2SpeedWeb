44e5711f6b5b24ccc66edae06cda5f993f8b62a8
Weuller Costa <weuller.costa@novahaus.com.br>
2022-09-03 19:09:55 -0300

    finishing template edition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { HomePageComponent } from './sections/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAccountComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
